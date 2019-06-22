6da6b1c599c2ee1e2d095e5c89608d6df8f3f582
Cheolmin Jo <webos21@gmail.com>
2019-06-22 21:37:39 +0900

    * xbionic_old  - working on Windows DLL begin/end

--- script/cmjo-bionic-gcc.bat
+++ script/cmjo-bionic-gcc.bat
@@ -5,4 +5,4 @@ REM You have to change the PATH & SPEC_DIR
 
 set PATH=C:\DevSoft\x64-4.8.1-release-posix-sjlj-rev5\bin;%PATH%
 
-"gcc" %* -specs "D:/WorkData/GitLocal/xbionic/ant/mingw32/emul/lib/cmjo-bionic-gcc-win.specs"
+"gcc" %* -specs "D:/WorkData/GitLocal/xbold/ant/mingw32/emul/lib/cmjo-bionic-gcc-win.specs"
